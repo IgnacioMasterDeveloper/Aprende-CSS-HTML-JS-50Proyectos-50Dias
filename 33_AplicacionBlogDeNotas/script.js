73e1cc9fc58ab120c1e7a4513b0c9d422daf06de
José Ignacio <ignaciomaster.dev@gmail.com>
2023-10-13 01:02:53 -0400

    Generador de contraseñas
    ejercicio 34 de 50

--- 33_AplicacionBlogDeNotas/script.js
+++ 33_AplicacionBlogDeNotas/script.js
@@ -30,21 +30,21 @@ function addNewNote(text = '') {
     textArea.value = text;
     main.innerHTML = marked(text);
 
-    deleteBtn.addEventListener('click', () =>{
+    deleteBtn.addEventListener('click', () => {
         note.remove();
 
         updateLS();
     })
 
-    editBtn.addEventListener('click', () =>{
+    editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden');
         textArea.classList.toggle('hidden')
     })
 
     textArea.addEventListener('input', (e) => {
-        const{value} = e.target;
+        const { value } = e.target;
 
-        main.innerHTML=marked(value);
+        main.innerHTML = marked(value);
 
         updateLS();
     })
